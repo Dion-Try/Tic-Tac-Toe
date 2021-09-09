e9ccca182037a07a8b80c0f59a54a21ee453ebe0
Dion-Try <61267319+Dion-Try@users.noreply.github.com>
2021-09-09 13:52:49 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.obnLnksBtn = new System.Windows.Forms.Button();
+            this.obnMitteBtn = new System.Windows.Forms.Button();
+            this.obnRechtsBtn = new System.Windows.Forms.Button();
+            this.mitteBtn = new System.Windows.Forms.Button();
+            this.mittRechtsBtn = new System.Windows.Forms.Button();
+            this.untenRechtsBtn = new System.Windows.Forms.Button();
+            this.untenMitteBtn = new System.Windows.Forms.Button();
+            this.mittLnksBtn = new System.Windows.Forms.Button();
+            this.untnLinksBtn = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.neustart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // obnLnksBtn
+            // 
+            this.obnLnksBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.obnLnksBtn.Location = new System.Drawing.Point(15, 13);
+            this.obnLnksBtn.Name = "obnLnksBtn";
+            this.obnLnksBtn.Size = new System.Drawing.Size(100, 100);
+            this.obnLnksBtn.TabIndex = 0;
+            this.obnLnksBtn.UseVisualStyleBackColor = false;
+            this.obnLnksBtn.Click += new System.EventHandler(this.obnLnksBtn_Click);
+            // 
+            // obnMitteBtn
+            // 
+            this.obnMitteBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.obnMitteBtn.Font = new System.Drawing.Font("Arial Nova Cond", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.obnMitteBtn.Location = new System.Drawing.Point(121, 13);
+            this.obnMitteBtn.Name = "obnMitteBtn";
+            this.obnMitteBtn.Size = new System.Drawing.Size(100, 100);
+            this.obnMitteBtn.TabIndex = 1;
+            this.obnMitteBtn.UseVisualStyleBackColor = false;
+            this.obnMitteBtn.Click += new System.EventHandler(this.obnMitteBtn_Click);
+            // 
+            // obnRechtsBtn
+            // 
+            this.obnRechtsBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.obnRechtsBtn.Location = new System.Drawing.Point(227, 13);
+            this.obnRechtsBtn.Name = "obnRechtsBtn";
+            this.obnRechtsBtn.Size = new System.Drawing.Size(100, 100);
+            this.obnRechtsBtn.TabIndex = 2;
+            this.obnRechtsBtn.UseVisualStyleBackColor = false;
+            this.obnRechtsBtn.Click += new System.EventHandler(this.obnRechtsBtn_Click);
+            // 
+            // mitteBtn
+            // 
+            this.mitteBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mitteBtn.Location = new System.Drawing.Point(121, 130);
+            this.mitteBtn.Name = "mitteBtn";
+            this.mitteBtn.Size = new System.Drawing.Size(100, 100);
+            this.mitteBtn.TabIndex = 3;
+            this.mitteBtn.UseVisualStyleBackColor = false;
+            this.mitteBtn.Click += new System.EventHandler(this.mitteBtn_Click);
+            // 
+            // mittRechtsBtn
+            // 
+            this.mittRechtsBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mittRechtsBtn.Location = new System.Drawing.Point(227, 130);
+            this.mittRechtsBtn.Name = "mittRechtsBtn";
+            this.mittRechtsBtn.Size = new System.Drawing.Size(100, 100);
+            this.mittRechtsBtn.TabIndex = 4;
+            this.mittRechtsBtn.UseVisualStyleBackColor = false;
+            this.mittRechtsBtn.Click += new System.EventHandler(this.mittRechtsBtn_Click);
+            // 
+            // untenRechtsBtn
+            // 
+            this.untenRechtsBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.untenRechtsBtn.Location = new System.Drawing.Point(227, 246);
+            this.untenRechtsBtn.Name = "untenRechtsBtn";
+            this.untenRechtsBtn.Size = new System.Drawing.Size(100, 100);
+            this.untenRechtsBtn.TabIndex = 5;
+            this.untenRechtsBtn.UseVisualStyleBackColor = false;
+            this.untenRechtsBtn.Click += new System.EventHandler(this.untenRechtsBtn_Click);
+            // 
+            // untenMitteBtn
+            // 
+            this.untenMitteBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.untenMitteBtn.Location = new System.Drawing.Point(121, 246);
+            this.untenMitteBtn.Name = "untenMitteBtn";
+            this.untenMitteBtn.Size = new System.Drawing.Size(100, 100);
+            this.untenMitteBtn.TabIndex = 7;
+            this.untenMitteBtn.UseVisualStyleBackColor = false;
+            this.untenMitteBtn.Click += new System.EventHandler(this.untenMitteBtn_Click);
+            // 
+            // mittLnksBtn
+            // 
+            this.mittLnksBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.mittLnksBtn.Location = new System.Drawing.Point(15, 130);
+            this.mittLnksBtn.Name = "mittLnksBtn";
+            this.mittLnksBtn.Size = new System.Drawing.Size(100, 100);
+            this.mittLnksBtn.TabIndex = 6;
+            this.mittLnksBtn.UseVisualStyleBackColor = false;
+            this.mittLnksBtn.Click += new System.EventHandler(this.mittLnksBtn_Click);
+            // 
+            // untnLinksBtn
+            // 
+            this.untnLinksBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.untnLinksBtn.Location = new System.Drawing.Point(15, 246);
+            this.untnLinksBtn.Name = "untnLinksBtn";
+            this.untnLinksBtn.Size = new System.Drawing.Size(100, 100);
+            this.untnLinksBtn.TabIndex = 8;
+            this.untnLinksBtn.UseVisualStyleBackColor = false;
+            this.untnLinksBtn.Click += new System.EventHandler(this.untnLinksBtn_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(438, 172);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 17);
+            this.label.TabIndex = 9;
+            // 
+            // neustart
+            // 
+            this.neustart.Location = new System.Drawing.Point(877, 118);
+            this.neustart.Name = "neustart";
+            this.neustart.Size = new System.Drawing.Size(75, 23);
+            this.neustart.TabIndex = 10;
+            this.neustart.Text = "Neustart";
+            this.neustart.UseVisualStyleBackColor = true;
+            this.neustart.Click += new System.EventHandler(this.neustart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1096, 780);
+            this.Controls.Add(this.neustart);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.untnLinksBtn);
+            this.Controls.Add(this.untenMitteBtn);
+            this.Controls.Add(this.mittLnksBtn);
+            this.Controls.Add(this.untenRechtsBtn);
+            this.Controls.Add(this.mittRechtsBtn);
+            this.Controls.Add(this.mitteBtn);
+            this.Controls.Add(this.obnRechtsBtn);
+            this.Controls.Add(this.obnMitteBtn);
+            this.Controls.Add(this.obnLnksBtn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button obnLnksBtn;
+        private System.Windows.Forms.Button obnMitteBtn;
+        private System.Windows.Forms.Button obnRechtsBtn;
+        private System.Windows.Forms.Button mitteBtn;
+        private System.Windows.Forms.Button mittRechtsBtn;
+        private System.Windows.Forms.Button untenRechtsBtn;
+        private System.Windows.Forms.Button untenMitteBtn;
+        private System.Windows.Forms.Button mittLnksBtn;
+        private System.Windows.Forms.Button untnLinksBtn;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button neustart;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+   
+    public partial class Form1 : Form
+    {
+
+        Spieler x = new Spieler("X");
+        Spieler o = new Spieler("O");
+        
+        public void aktivSetzen()
+        {
+            if (x.aktiv)
+            {
+                x.aktiv = false;
+                o.aktiv = true;
+            }
+            else
+            {
+                o.aktiv = false;
+                x.aktiv = true;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            x.aktiv = true;
+        }
+
+        private void obnLnksBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                obnLnksBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                obnLnksBtn.Text = o.name;
+                aktivSetzen();
+            }
+            obnLnksBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void obnMitteBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                obnMitteBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                obnMitteBtn.Text = o.name;
+                aktivSetzen();
+            }
+            obnMitteBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void obnRechtsBtn_Click(object sender, EventArgs e)
+        {
+                if (x.aktiv)
+                {
+                    obnRechtsBtn.Text = x.name;
+                    aktivSetzen();
+                }
+                else
+                {
+                    obnRechtsBtn.Text = o.name;
+                    aktivSetzen();
+                }
+            obnRechtsBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void mittLnksBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                mittLnksBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                mittLnksBtn.Text = o.name;
+                aktivSetzen();
+            }
+            mittLnksBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void mitteBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                mitteBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                mitteBtn.Text = o.name;
+                aktivSetzen();
+            }
+            mitteBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void mittRechtsBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                mittRechtsBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                mittRechtsBtn.Text = o.name;
+                aktivSetzen();
+            }
+            fallsGewonnen();
+            mittRechtsBtn.Enabled = false;
+        }
+
+        private void untnLinksBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                untnLinksBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                untnLinksBtn.Text = o.name;
+                aktivSetzen();
+            }
+            untnLinksBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void untenMitteBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                untenMitteBtn.Text = x.name;
+                aktivSetzen();
+            }
+            else
+            {
+                untenMitteBtn.Text = o.name;
+                aktivSetzen();
+            }
+            untenMitteBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void untenRechtsBtn_Click(object sender, EventArgs e)
+        {
+            if (x.aktiv)
+            {
+                untenRechtsBtn.Text = x.name;
+                aktivSetzen();
+                
+            }
+            else
+            {
+                untenRechtsBtn.Text = o.name;
+                aktivSetzen();
+
+            }
+            untenRechtsBtn.Enabled = false;
+            fallsGewonnen();
+        }
+
+        private void fallsGewonnen()
+        {
+            // oben waagrecht
+            if (obnLnksBtn.Text == "X" && obnMitteBtn.Text =="X" && obnRechtsBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnLnksBtn.Text == "O" && obnMitteBtn.Text == "O" && obnRechtsBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // mitte waagrecht
+            else if (mittLnksBtn.Text == "X" && mitteBtn.Text == "X" && mittRechtsBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (mittLnksBtn.Text == "O" && mitteBtn.Text == "O" && mittRechtsBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // unten waagrecht
+            else if (untnLinksBtn.Text == "X" && untenMitteBtn.Text == "X" && untenRechtsBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (untnLinksBtn.Text == "O" && untenMitteBtn.Text == "O" && untenRechtsBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // links vertikal
+            else if (obnLnksBtn.Text == "X" && mittLnksBtn.Text == "X" && untnLinksBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnLnksBtn.Text == "O" && mittLnksBtn.Text == "O" && untnLinksBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // mitte vertikal
+            else if (obnMitteBtn.Text == "X" && mitteBtn.Text == "X" && untenMitteBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnMitteBtn.Text == "O" && mitteBtn.Text == "O" && untenMitteBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // rechts vertikal
+            else if (obnRechtsBtn.Text == "X" && mittRechtsBtn.Text == "X" && untenRechtsBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnRechtsBtn.Text == "O" && mittRechtsBtn.Text == "O" && untenRechtsBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // Links nach rechts diagonal
+            else if (obnLnksBtn.Text == "X" && mitteBtn.Text == "X" && untenRechtsBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnLnksBtn.Text == "O" && mitteBtn.Text == "O" && untenRechtsBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            // Recths nach links diagonal
+            else if (obnRechtsBtn.Text == "X" && mitteBtn.Text == "X" && untnLinksBtn.Text == "X")
+            {
+                labelText(0);
+            }
+            else if (obnRechtsBtn.Text == "O" && mitteBtn.Text == "O" && untnLinksBtn.Text == "O")
+            {
+                labelText(1);
+            }
+            else if(obnLnksBtn.Enabled == false && obnMitteBtn.Enabled == false && obnRechtsBtn.Enabled == false && mittLnksBtn.Enabled == false && mitteBtn.Enabled == false && mittRechtsBtn.Enabled == false && untnLinksBtn.Enabled == false && untenMitteBtn.Enabled == false && untenRechtsBtn.Enabled == false)
+            {
+                labelText(2);
+            }
+        }
+        public void labelText(int resultat)
+        {
+            switch (resultat)
+            {
+                // 0 = x hat gewonnen
+                case 0:
+                    label.Text = "Spieler x hat Spieler O zerstört";
+                    break;
+                // 1 = o hat gewonnen
+                case 1:
+                    label.Text = "Spieler O hat Spieler X zerstört";
+                    break;
+                // 2 = unentschieden
+                case 2:
+                    label.Text = "Unentschieden";
+                    break;
+            }
+            obnLnksBtn.Enabled = false;
+            obnMitteBtn.Enabled = false;
+            obnRechtsBtn.Enabled = false;
+            mittLnksBtn.Enabled = false;
+            mitteBtn.Enabled = false;
+            mittRechtsBtn.Enabled = false;
+            untnLinksBtn.Enabled = false;
+            untenMitteBtn.Enabled = false;
+            untenRechtsBtn.Enabled = false;
+
+
+        }
+
+        private void neustart_Click(object sender, EventArgs e)
+        {
+            obnLnksBtn.Enabled = true;
+            obnMitteBtn.Enabled = true;
+            obnRechtsBtn.Enabled = true;
+            mittLnksBtn.Enabled = true;
+            mitteBtn.Enabled = true;
+            mittRechtsBtn.Enabled = true;
+            untnLinksBtn.Enabled = true;
+            untenMitteBtn.Enabled = true;
+            untenRechtsBtn.Enabled = true;
+
+            obnLnksBtn.Text = "";
+            obnMitteBtn.Text = "";
+            obnRechtsBtn.Text = "";
+            mittLnksBtn.Text = "";
+            mitteBtn.Text = "";
+            mittRechtsBtn.Text = "";
+            untnLinksBtn.Text = "";
+            untenMitteBtn.Text = "";
+            untenRechtsBtn.Text = "";
+
+            label.Text = "";
+
+            aktivSetzen();
+
+
+        }
+    }
+
+    public class Spieler
+    {
+        public string name;
+
+        public bool aktiv = false;
+
+        public Spieler(string name)
+        {
+            this.name = name;
+        }
+    }
+}
